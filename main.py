dd914fef137c526a053f3dc92cea4de56a19c498
Dizzle98 <leon.duellmann@gmail.com>
2026-07-23 19:32:02 +0200

    feat: add openrouter api

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("no API_KEY found")
+
+    client = OpenAI(
+        base_url = "https://openrouter.ai/api/v1",
+        api_key = api_key
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ]
+
+    response = client.chat.completions.create(model="openrouter/free", messages=messages)
+
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
