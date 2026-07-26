ead67460d89e8a854c4dcabfbdf83f781ae0f129
Dizzle98 <leon.duellmann@gmail.com>
2026-07-26 19:18:57 +0200

    feat: add list dir

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,10 +10,22 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-        if not os.path.isdir(directory):
+        if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
     except Exception as e:
         return f'Error: "{e}"'
 
+    try:
+        dir_content = os.listdir(target_dir)
+        results = [f"Result for '{directory}' directory:"]
+        for content in dir_content:
+            file_name = content
+            file_size = os.path.getsize(os.path.join(target_dir, file_name))
+            is_directory = os.path.isdir(os.path.join(target_dir, file_name))
+            results.append(f"- {file_name}: file_size={file_size} bytes, is_dir={is_directory}")
+    except Exception as e:
+        return f'Error: "{e}"'
 
-    return f'Success: "{directory}" is within the working directory'
+    result = f"{file_name}: file_size={file_size} bytes, is_dir={is_directory}"
+
+    return "\n".join(results) #f'Success: "{directory}" is within the working directory'
